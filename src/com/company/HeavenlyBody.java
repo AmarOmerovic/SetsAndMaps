304cc3e12e90ed9d064b0bf7a9f95a711071786a
amaromerovic <amaromerovic473@gmail.com>
2022-11-06 21:31:48 +0100

    Initial commit

---
+++ src/com/company/HeavenlyBody.java
@@ -0,0 +1,106 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class HeavenlyBody {
+    private final Key key;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public enum BodyTypes{
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+        this.key = new Key(name, bodyType);
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public boolean addSatellite(HeavenlyBody moon){
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+
+    @Override
+    public final boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+
+        if (obj instanceof HeavenlyBody){
+            HeavenlyBody theObject = (HeavenlyBody) obj;
+            return this.key.equals(theObject.getKey());
+        }
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return this.key.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.key.name + ": " + this.key.bodyTypes + ", " + this.orbitalPeriod;
+    }
+
+    public static final class Key{
+        private final String name;
+        private final BodyTypes bodyTypes;
+
+        private Key(String name, BodyTypes bodyTypes) {
+            this.name = name;
+            this.bodyTypes = bodyTypes;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyTypes() {
+            return bodyTypes;
+        }
+
+        @Override
+        public int hashCode() {
+            return (this.getName().hashCode() + this.getBodyTypes().hashCode()) % 10;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+            if (this.name.equals(key.getName())){
+                return (this.getBodyTypes() == key.getBodyTypes());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": " + this.bodyTypes;
+        }
+
+        public static Key makeKey(String name, BodyTypes bodyTypes){
+            return new Key(name, bodyTypes);
+        }
+
+    }
+}
